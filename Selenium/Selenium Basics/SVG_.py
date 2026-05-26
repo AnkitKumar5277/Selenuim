5f10b20abcb7f717da58f06d4a0e0dc616c3e039
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-05-26 22:45:32 +0530

    Update SVG_.py

--- Selenium/Selenium Basics/SVG_.py
+++ Selenium/Selenium Basics/SVG_.py
@@ -7,17 +7,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
-
 @allure.title("SVG")
 @allure.description("Verify svg")
 def test_verify_svg():
     driver = webdriver.Chrome()
     driver.get("https://www.amcharts.com/svg-maps/?map=india")
-
-    #name() or local-name()
-
-    # //*[name()='svg']/*[name()='g'][7]/*[name()='g']/*[name()='g']/*[name()='path']
-
     list_of_states = driver.find_elements(By.XPATH,
                                           "//*[name()='svg']/*[name()='g'][7]/*[name()='g']/*[name()='g']/*[name()='path']")
     for state in list_of_states:
@@ -25,6 +19,4 @@ def test_verify_svg():
         if "Tripura" in state.get_attribute("aria-label"):
             state.click()
             break
-
-
     time.sleep(5)
